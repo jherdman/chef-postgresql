d6e491fe4432d02b4db8fd8e89799d07e84dc3b3
James Herdman <james.herdman@me.com>
2013-09-30 10:55:34 -0400

    Temporary fix for #52

--- recipes/service.rb
+++ recipes/service.rb
@@ -7,6 +7,14 @@ file "/usr/sbin/policy-rc.d" do
   action :delete
 end
 
+# FIXME Hack hack hack... https://github.com/phlipper/chef-postgresql/issues/52
+directory "/var/run/postgresql" do
+  owner "postgres"
+  group "postgres"
+  mode 2775
+  action :create
+end
+
 # define the service
 service "postgresql" do
   supports restart: true
